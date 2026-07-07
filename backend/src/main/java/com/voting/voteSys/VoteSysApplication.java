df42666fdccb0d9d1707899c58194782fc55a411
roushan-jha <jharoushan20@gmail.com>
2026-07-07 12:46:58 +0530

    Initial commit: Spring Boot backend setup

---
+++ backend/src/main/java/com/voting/voteSys/VoteSysApplication.java
@@ -0,0 +1,13 @@
+package com.voting.voteSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSysApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/voting/voteSys/VoteSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.voteSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
